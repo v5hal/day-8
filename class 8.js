e69e89600ed738f1dc7574a37b7ef85f0c2d4628
vishal <107488823+v5hal@users.noreply.github.com>
2022-09-23 20:57:59 +0530

    Add files via upload

---
+++ class 8.js
@@ -0,0 +1,64 @@
+
+var request = new XMLHttpRequest();
+
+request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
+
+request.send();
+
+request.onload=function(){
+var data = JSON.parse(request.response)
+console.log(data);
+
+
+// Get all the countries from the Asia continent /region using the Filter function
+
+
+var result = data.filter((ele)=>ele.region==="Asia")
+console.log(result);
+     
+
+// Get all the countries with a population of less than 2 lakhs using Filter function
+
+// var members= data.filter((ele)=>{
+//     return ele.population <=20000
+   
+// })
+
+// console.log(members);
+
+// Print the following details name, capital, flag using forEach function
+
+
+//    data.forEach((element,index)=>{
+//     data[index]= element.capital+""+ element.name+""+element.flag
+   
+//                                  })
+
+// console.log(data);
+
+
+
+// Print the total population of countries using reduce function
+
+
+var member= data.reduce((ele)=>{
+     data= ele.capital
+
+})
+
+console.log(member);
+
+
+// Print the country which uses US Dollars as currency.
+
+
+   for ( var i=0; i<data.length; i++){
+           console.log(data[i].name,data[i].currencies);
+   
+                   }   
+
+
+
+}
+
+
