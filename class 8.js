f479a20a8d6d780127449904ded9119c28a3b824
vishal <107488823+v5hal@users.noreply.github.com>
2022-10-18 21:58:45 +0530

    Update class 8.js

--- class 8.js
+++ class 8.js
@@ -1,63 +1,97 @@
+// oops task
 
-var request = new XMLHttpRequest();
+// class constructor
 
-request.open('GET','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json',true);
+// class person{
+//     constructor(moviename,studioname,rating)    
+//     {
+//         this.moviename= moviename;
+//         this.studioname= studioname;
+//         this.rating= rating;
+//     }
+//     getdetails(){
+//         return(`the name of the movie is ${this.moviename} shows in ${this.studioname} and movie rating ${this.rating}`)
+//     }
+// }
 
-request.send();
+// let person1 = new person("vikram","ashok theater","5 star");
 
-request.onload=function(){
-var data = JSON.parse(request.response)
-console.log(data);
-
-
-// Get all the countries from the Asia continent /region using the Filter function
-
-
-var result = data.filter((ele)=>ele.region==="Asia")
-console.log(result);
-     
-
-// Get all the countries with a population of less than 2 lakhs using Filter function
-
-// var members= data.filter((ele)=>{
-//     return ele.population <=20000
-   
-// })
-
-// console.log(members);
-
-// Print the following details name, capital, flag using forEach function
-
-
-//    data.forEach((element,index)=>{
-//     data[index]= element.capital+""+ element.name+""+element.flag
-   
-//                                  })
-
-// console.log(data);
+// console.log(person1.getdetails());
 
 
 
-// Print the total population of countries using reduce function
+// d) Write a piece of code that creates an instance of the class 
+// Movie with the title “Casino Royale”, the studio “Eon Productions”, and the rating “PG-13”
 
 
-var member= data.reduce((acc,ele)=> acc+ele.population,0);
+// class detail{
+//     constructor(moviename,studioname,rating)
+//     {
+//         this.moviename= moviename;
+//         this.studioname= studioname;
+//         this.rating= rating;
+//     }
+//     getdetails(){
+//         return(`movie with the title ${this.moviename}, the studio ${this.studioname} and the rating ${this.rating}`)
+//     }
+// }
 
-console.log(member)
+// let detail1 = new detail("Casino Royale","Eon Production","PG13");
+
+// console.log(detail1.getdetails());
 
 
-// Print the country which uses US Dollars as currency.
+// Write a “person” class to hold all the details.
 
-let cur=data.filter((ele) => {   
-for(let i in ele.currencies){
-        if(ele.currencies[i].code==="USD"){
-                return true;
-        }
-}
-}).map((ele)=>ele.name)
+// class person {
+//     constructor(firstname,lastname,age,place)
+//     {
+//         this.firstname = firstname;
+//         this.lastname = lastname;
+//         this.age = age;
+//         this.place = place;
+//     }
+//     getdetails(){
+//         return(`the name of a person is ${this.firstname} ${this.lastname} with age ${this.age}born at ${this.place}`)
+//     }
+// }
 
-console.log(cur);
+// let person1 = new person("guvi","geek","18","chennai");
+
+// let person2 = new person("vishal","deepa","21","kerala");
+
+// console.log(person1.getdetails());
 
 
-}
+// write a class to calculate the uber price.
 
+// class uber {
+//     constructor(distance,rate,waitingperiod)
+//     {
+//         this.distance = distance;
+//         this.rate = rate;
+//         this.waitingperiod = waitingperiod;
+//     }
+
+// setDistance(dist){
+//     this.distance=dist;
+// }
+
+// getRideDetails(){
+//     return(`the distance of the rider is ${this.distance} and rate of ₹${this.rate} with waitingperiod of ${this.waitingperiod}`)
+// }
+
+
+// getprice(){
+//      let totalprice =(this.rate* this.distance)+(this.waitingperiod*5);
+//     return totalprice
+// }
+// }
+
+// let uber1= new uber(350,20,15);
+
+// console.log(uber1.getprice());
+
+// uber1.setDistance(150);
+
+// console.log(uber1.getprice())
